d97ca02fd457ab51310344b7feac98d0aa79e74b
abdulkarim <abdulkarim.saddik@gmail.com>
2022-12-10 22:19:43 +0100

    Task2: all unit test passed
    I tested the code on a separate script and it worked by me
    
    add comments to database.py

--- models.py
+++ models.py
@@ -32,33 +32,49 @@ class NearEarthObject:
     initialized to an empty collection, but eventually populated in the
     `NEODatabase` constructor.
     """
-    def __init__(self, designation, name=None, diameter=float('nan'), hazardous=False, **approaches):
+    def __init__(self, designation='{fullname}', name=None, diameter=float('nan'), hazardous='', approaches=[]):
         """Create a new `NearEarthObject`.
 
         :param info: A dictionary of excess keyword arguments supplied to the constructor.
         """
         self.designation = str(designation)
-        self.name = name
-        self.diameter = float(diameter)
-        self.hazardous = hazardous
+        if not bool(name):
+            self.name = None
+        else:
+            self.name = name
+
+        self._diameter = {'diameter': ''}
+        if diameter == '':
+            self.diameter = float('nan')
+            self._diameter['diameter'] = '{diameter}'
+
+        else:
+            self.diameter = float(diameter)
+            self._diameter['diameter'] = self.diameter
+        self.str_hazardous = ''
+
+        self.hazardous = False
+        if hazardous == 'Y' or hazardous == 'y':
+            self.str_hazardous = 'is'
+            self.hazardous = True
+        elif hazardous == 'N' or hazardous == 'N' or hazardous == '':
+            self.str_hazardous = 'is not'
+        else:
+            raise ValueError("non supported input value for hazardous!")
 
         # Create an empty initial collection of linked approaches.
         self.approaches = approaches
 
     @property
     def fullname(self):
-        if self.name:
-            return self.designation + ' ' + self.name
-        else:
+        if not bool(self.name):
             return self.designation
+        return self.designation + ' (' + self.name + ')'
 
     def __str__(self):
         """Return `str(self)`."""
-        str_hazardous = 'is not'
-        if self.hazardous:
-            str_hazardous = 'is'
-        return f"NEO {self.fullname} has a diameter of {self.diameter:.3f} km " \
-               f"and {str_hazardous} potentially hazardous."
+        return f"NEO {self.fullname} has a diameter of {self._diameter['diameter']:.3f} km " \
+               f"and {self.str_hazardous} potentially hazardous."
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
@@ -79,18 +95,34 @@ class CloseApproach:
     private attribute, but the referenced NEO is eventually replaced in the
     `NEODatabase` constructor.
     """
-    def __init__(self, time, distance, velocity, neo):
+    def __init__(self, designation='', time='', distance=float('nan'), velocity=float('nan'), neo=NearEarthObject()):
         """Create a new `CloseApproach`.
 
         :param info: A dictionary of excess keyword arguments supplied to the constructor.
         """
-        self._designation = neo.designation
-        self.time = cd_to_datetime(time)
-        self.distance = distance
-        self.velocity = velocity
-
-        # Create an attribute for the referenced NEO, originally None.
+        self.designation = designation
+        self.time = None
         self.neo = neo
+        if time:
+            self.time = cd_to_datetime(time)
+
+        self._distance = {'distance': ''}
+        if distance == '':
+            self.distance = float('nan')
+            self._distance['distance'] = '{distance}'
+
+        else:
+            self.distance = float(distance)
+            self._distance['distance'] = self.distance
+
+        self._velocity = {'velocity': ''}
+        if velocity == '':
+            self.velocity = float('nan')
+            self._velocity['velocity'] = '{velocity}'
+
+        else:
+            self.velocity = float(velocity)
+            self._velocity['velocity'] = self.velocity
 
     @property
     def time_str(self):
@@ -105,14 +137,17 @@ class CloseApproach:
         formatted string that can be used in human-readable representations and
         in serialization to CSV and JSON files.
         """
+        if not bool(self.time):
+            return '{time_str}'
         return datetime_to_str(self.time)
 
     def __str__(self):
         """Return `str(self)`."""
-        return f"on {self.time_str!r}, distance={self.distance:.2f}, " \
-               f"velocity={self.velocity:.2f}, neo={self.neo!r})"
+        return f"on {self.time_str!r} {self.neo.fullname} approaches Earth at a distance of " \
+               f"{self._distance['distance']:.2f} au and " \
+               f"velocity of {self._velocity['velocity']:.2f} km/s"
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
-        return f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, " \
-               f"velocity={self.velocity:.2f}, neo={self.neo!r})"
+        return f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f} au, " \
+               f"and a velocity of {self.velocity:.2f})"
