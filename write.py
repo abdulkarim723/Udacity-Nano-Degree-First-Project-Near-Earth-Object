271519d3b3e42d1195c73e086df75f3a58e9d4e8
abdulkarim <abdulkarim.saddik@gmail.com>
2022-12-18 19:42:03 +0100

    Task4: writing results to csv or json file

--- write.py
+++ write.py
@@ -28,7 +28,18 @@ def write_to_csv(results, filename):
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+
+    with open(filename, 'w', newline='') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames)
+        writer.writeheader()
+        rows = []
+        for elem in results:
+            if elem.neo.name is None:
+                elem.neo.name = ''
+            row = {fieldnames[0]: elem.time_str, fieldnames[1]: elem.distance, fieldnames[2]: elem.velocity,
+                   fieldnames[3]: elem.designation,
+                   fieldnames[4]: elem.neo.name, fieldnames[5]: elem.neo.diameter, fieldnames[6]: elem.neo.hazardous}
+            writer.writerow(row)
 
 
 def write_to_json(results, filename):
@@ -42,4 +53,22 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    with open(filename, 'w', newline='\n') as outfile:
+        data = []
+        for result in results:
+            approach_dict = {}
+            approach_neo = {}
+            approach_dict['datetime_utc'] = result.time_str
+            approach_dict['distance_au'] = result.distance
+            approach_dict['velocity_km_s'] = result.velocity
+
+            approach_neo['designation'] = result.neo.designation
+            approach_neo['name'] = result.neo.name
+            if approach_neo['name'] is None:
+                approach_neo['name'] = ''
+            approach_neo['diameter_km'] = result.neo.diameter
+            approach_neo['potentially_hazardous'] = result.neo.hazardous
+
+            approach_dict['neo'] = approach_neo
+            data.append(approach_dict)
+        json.dump(data, outfile, default=str, indent=2)
